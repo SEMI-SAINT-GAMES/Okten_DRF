9abd0fb84c2eb9bfe602bab704365b7ca1071e5b
SEMI-SAINT-GAMES <120226332+SEMI-SAINT-GAMES@users.noreply.github.com>
2023-10-19 21:31:34 +0300

    5 not done

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-
+from django.db.transaction import atomic
 from apps.users.models import ProfileModel
 
 UserModel = get_user_model()
 
-class ProfileSerializer(serializers.Serializer):
+class ProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProfileModel
@@ -23,6 +23,7 @@ class UserSerializer(serializers.ModelSerializer):
                 'write_only': True
             }
         }
+    @atomic
     def create(self, validated_data: dict):
         profile = validated_data.pop('profile')
         profile = ProfileModel.objects.create(**profile)
